8eb28db6896ab186ad7a77b6fe887dfd0a5c1725
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2015-11-30 10:59:54 -0200

    removing wrong application

--- mobile/startup/git.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf /var/www
-cd /var && git clone --depth 1 https://17e236f9204a6e98c65200da779e2a684c995a5d@github.com/mesalva/api.git www && cd www && COMPOSER_HOME=/var/www php composer.phar selfupdate && COMPOSER_HOME=/var/www php composer.phar update
-
